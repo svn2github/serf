68619932978602d575fd8c076889c916a5cecc3d
rhuijben <rhuijben@13f79535-47bb-0310-9956-ffa450edef68>
2015-09-01 09:27:07 +0000

    Checkpoint buildbot scripts
    git-svn-id: https://svn.apache.org/repos/asf/serf@1700439 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/buildbot/windows/serftest-test.cmd
@@ -0,0 +1,41 @@
+@ECHO off
+REM ================================================================
+REM   Licensed to the Apache Software Foundation (ASF) under one
+REM   or more contributor license agreements.  See the NOTICE file
+REM   distributed with this work for additional information
+REM   regarding copyright ownership.  The ASF licenses this file
+REM   to you under the Apache License, Version 2.0 (the
+REM   "License"); you may not use this file except in compliance
+REM   with the License.  You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM   Unless required by applicable law or agreed to in writing,
+REM   software distributed under the License is distributed on an
+REM   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM   KIND, either express or implied.  See the License for the
+REM   specific language governing permissions and limitations
+REM   under the License.
+REM ================================================================
+
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
+CALL ..\serf-config.cmd
+IF ERRORLEVEL 1 EXIT /B 1
+
+SET BB=%CD:~,-6%
+SET INTDIR=%BB%\deps\build\release
+SET INSTALL=%CD%\install
+
+IF NOT EXIST "%INSTALL%\" MKDIR "%INSTALL%"
+
+PATH %PATH%;%BB%\deps\release\bin;%BB%\deps\build\scons\scripts
+SET PYTHONPATH=%BB%\deps\build\scons\Lib\site-packages\scons
+
+SET SA=PREFIX=%INSTALL% OPENSSL=%INTDIR% ZLIB=%INTDIR% ZLIB=%INTDIR% APR=%INTDIR% APU=%INTDIR% SOURCE_LAYOUT=no TARGET_ARCH=win32 MSVC_VERSION=10.0 APR_STATIC=yes
+
+echo scons %SA% DEBUG=yes check
+CALL scons.bat %SA% DEBUG=yes check
+IF ERRORLEVEL 1 EXIT /B 1
+
+EXIT /B 0
